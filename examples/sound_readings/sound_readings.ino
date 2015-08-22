6d6a46ad854734eafc0cefdbc79bb9adc6f67a83
Charlie Gorichanaz <charlie@gorichanaz.com>
2015-08-22 03:57:22 -0700

    Sound readings to serial for CSV

---
+++ examples/sound_readings/sound_readings.ino
@@ -0,0 +1,35 @@
+/*
+  This program takes FHT_N readings from an attached microphone and then prints 
+  them separated by commas on a single line. The output can be copied into a 
+  text file and saved as a .CSV file for subsequent opening in a spreadsheet 
+  application. 
+
+  Tested on Adafruit FLORA (https://www.adafruit.com/products/659)
+*/
+
+
+#define MIC_PIN A9  // Microphone is attached to this analog pin
+#define FHT_N   256 // typical FHT function needs 256 readings
+int fht_input[256], i;
+
+
+void setup() {
+  while (!Serial); // wait for serial monitor to be opened
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  cli(); // disable interrupts
+  for (i=0; i<FHT_N; i++) {
+    fht_input[i] = analogRead(MIC_PIN);
+  }
+  sei(); // reenable interrupts
+  
+  for (i=0; i<FHT_N; i++) {
+    Serial.print(fht_input[i]);Serial.print(",");
+  }
+  Serial.println();
+  
+  delay(1000);
+}
