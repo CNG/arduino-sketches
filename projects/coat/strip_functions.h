d963b4fd8cb142e816eed6795ba928e65734997c
Charlie Gorichanaz <charlie@gorichanaz.com>
2016-08-16 21:46:08 -0700

    Repurpose stick as coat, convert to matrix (Burning Man 2016)

---
+++ projects/coat/strip_functions.h
@@ -0,0 +1,27 @@
+#include <Adafruit_NeoPixel.h> // don't need functions but defines some types
+
+extern bool pushed;
+extern volatile uint16_t opt;
+extern uint8_t _pin_lev;
+extern uint8_t _pin_opt;
+extern int lightMode;
+
+extern void setVerticalPixelColor(uint16_t n, uint32_t c);
+extern void setHorizontalPixelColor(uint16_t n, uint32_t c);
+extern void setPixelColor(uint16_t n, uint32_t c);
+extern int numPixels();
+extern void show();
+extern void setBrightness(uint8_t n);
+extern uint16_t color(uint8_t r, uint8_t g, uint8_t b);
+
+void buttonPushed();
+void setBrightness();
+int getOpt(uint8_t pin, int min, int max);
+
+void colorWipe(uint32_t c, uint8_t wait);
+void pulse(int w);
+void rainbow();
+void rainbowCycle();
+void rainbowSingle();
+
+uint32_t Wheel(byte WheelPos);
