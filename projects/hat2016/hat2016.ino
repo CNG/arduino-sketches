b14605181bc66dd1dbf3578e97f43450c28a2358
Charlie Gorichanaz <charlie@gorichanaz.com>
2017-05-13 00:19:51 -0700

    Add hat2016

---
+++ projects/hat2016/hat2016.ino
@@ -0,0 +1,194 @@
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
+/*
+  TODO (this includes nonsettings)
+  Hardware and connection dependent settings
+ */
+
+//int lightMode = -1; // one less than we want bc interrupt runs at beginning apparently
+int lightMode = 3; // one less than we want bc interrupt runs at beginning apparently
+
+#include "strip_functions.h"
+int _num_led_cols = 2; // Number of columns of LEDs
+int _num_led_rows = 90; // Number of pixels in each column
+uint8_t _pin_led = 1; // NeoPixel LED strand is connected to this pin
+uint8_t _pin_lev = A9; // using 1K potentiometer
+uint8_t _pin_opt = A11; // using 1K potentiometer
+extern boolean checkButton();
+
+#include "sound_reactive.h"
+uint8_t _pin_mic = A7;  // Microphone is attached D6
+extern int vol;
+
+
+/* Initialize variables */
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(
+  _num_led_rows, _num_led_cols, _pin_led,
+  NEO_MATRIX_BOTTOM  + NEO_MATRIX_RIGHT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB + NEO_KHZ800);
+Adafruit_NeoMatrix strip = matrix;
+
+boolean pushed = false;
+/**
+ * Instead doing work directly within interrupt, we'll just toggle a
+ * variable that we'll check for later. This prevents any weirdness due to 
+ * variable volatility.
+ */
+void buttonInterrupt() {
+  pushed = true;
+}
+
+
+void setup() {
+
+  /* Interrupt setup */
+
+    /*
+      "FLORA supports external interrupts on D0 (INT2), D1(INT3), D2(INT1), and 
+      D3(INT0). You would attach your button between, say, D0 and GND."
+      FLORA's D0 is labeled "RX" (https://learn.adafruit.com/assets/2845)
+      and corresponds to Arduino interrupt 2.
+      * https://www.arduino.cc/en/Reference/PinMode
+      * https://www.arduino.cc/en/Reference/attachInterrupt
+      * https://forums.adafruit.com/viewtopic.php?f=25&t=57650:
+     */
+    pinMode(0, INPUT_PULLUP); // configure pin to behave as input
+    // when button is pushed, run buttonPushed()
+    attachInterrupt(2, buttonInterrupt, FALLING); // listen for high to low
+
+    // from Amplitie code, not sure if needed or what it does
+    //pinMode(_pin_led, OUTPUT);
+
+    // http://www.avrfreaks.net/forum/timer1-atmega32u4-arduino-leonardo
+    cli();          // disable global interrupts
+
+    TCCR1A = 0;     // set entire TCCR1A register to 0
+    TCCR1B = 0;     // same for TCCR1B
+    // set compare match register to desired timer count
+    //OCR1A = 781; // 10 times a second
+    OCR1A = 78; // 100 times a second
+    //OCR1A = 8; // 1000 times a second
+    // turn on CTC mode:
+    TCCR1B |= (1 << WGM12);
+    // Set CS10 and CS12 bits for 1024 prescaler
+    TCCR1B |= (1 << CS10);
+    TCCR1B |= (1 << CS12);
+    // enable timer compare interrupt
+    TIMSK1 |= (1 << OCIE1A);
+
+    TCCR3A = 0;     // set entire TCCR1A register to 0
+    TCCR3B = 0;     // same for TCCR1B
+    // set compare match register to desired timer count
+    //OCR3A = 781; // 10 times a second
+    OCR3A = 78; // 100 times a second
+    //OCR3A = 8; // 1000 times a second
+    // turn on CTC mode:
+    TCCR3B |= (1 << WGM12);
+    // Set CS10 and CS12 bits for 1024 prescaler
+    TCCR3B |= (1 << CS10);
+    TCCR3B |= (1 << CS12);
+    // enable timer compare interrupt
+    TIMSK3 |= (1 << OCIE1A);
+
+    // enable global interrupts
+    sei();
+  
+  /* sound_reactive */
+
+    memset((void*) vol, 0, sizeof(vol));
+
+    /*
+      This is only needed on 5V Arduinos (Uno, Leonardo, etc.). Connect 3.3V to 
+      mic AND TO AREF ON ARDUINO and enable this line.  Audio samples are 
+      'cleaner' at 3.3V. COMMENT OUT THIS LINE FOR 3.3V ARDUINOS (FLORA, ETC.): 
+     */
+    // analogReference(EXTERNAL);
+
+  matrix.begin();
+  matrix.show(); // Initialize all pixels to 'off'
+
+  //while (!Serial);
+  Serial.begin(9600);
+
+}
+
+
+volatile uint16_t opt = 0;
+SIGNAL(TIMER1_COMPA_vect) {
+  opt = analogRead(_pin_opt);
+}
+volatile bool flag = false;
+SIGNAL(TIMER3_COMPA_vect) {
+  flag = true;
+}
+
+
+void setVerticalPixelColor(uint16_t n, uint32_t c){
+  for(uint8_t i = 0; i < _num_led_cols; i++) {
+    matrix.drawPixel(n, i, c);
+  }
+}
+void setHorizontalPixelColor(uint16_t n, uint32_t c){
+  for(uint8_t i = 0; i < _num_led_rows; i++) {
+    matrix.drawPixel(n, i, c);
+  }
+}
+void setPixelColor(uint16_t n, uint32_t c){
+  setVerticalPixelColor(n, c);
+}
+int numPixels(){
+  return _num_led_rows;
+}
+void show(){
+  matrix.show();
+}
+void setBrightness(uint8_t n){
+  matrix.setBrightness(n);
+}
+uint16_t color(uint8_t r, uint8_t g, uint8_t b){
+  matrix.Color(r, g, b);
+}
+
+void loop() {
+
+  checkButton();
+
+  Serial.println( getOpt(_pin_lev, 0, 255));
+
+  switch (lightMode) {
+    default:
+      // we exceeded defined modes, reset and fall through
+      lightMode = 0;
+    case 0:
+      colorWipe(strip.Color(0, 0, 0), 0);
+      delay(20);
+      break;
+    case 1: rainbowSingle(); break;
+    case 2: rainbowCycle(); break;
+    case 3: rainbowCycle2(); break;
+    case 4:
+      pulse(0);
+      break;
+    case 5:
+      //volMeter(120,220);
+      if( flag ){
+        flag = false;
+        volMeter(80,150);
+      }
+      break;
+    case 6:
+      //volMeter(120,220);
+      if( flag ){
+        flag = false;
+        volMeter(120,220);
+      }
+      break;
+    //case 5: volMeter(220,250); break;
+    //case 6: volMeter( 20, 60); break;
+    //case 7: volMeter(  1,255); break;
+  }
+
+}
+
---
+++ projects/hat2016/strip_functions.cpp
@@ -0,0 +1,292 @@
+#include "strip_functions.h"
+
+/**
+ * See if button was pushed. If so, delay and then set "pushed" back to false.
+ * If we don't delay first, the interrupt might set "pushed" back to true, 
+ * causing the condition to fire too many times on "one" button push.
+ */
+bool checkButton() {
+  if( pushed ){
+    setBrightness();
+    delay(250);
+    pushed = false;
+    lightMode++;
+    return true;
+  }
+  return false;
+}
+
+/*
+  Get option based on potentiometer value
+ */
+int getOpt(uint8_t pin, int min, int max) {
+  // convert analogRead's range of 0 to 1023 to option range min to max
+  return map(analogRead(pin), 0, 1023, min, max);
+}
+
+/*
+  Set strip brightness based on potentiometer value
+ */
+void setBrightness() {
+  // convert analogRead's range of 0 to 1023 to brightness range 0 to 255
+  uint8_t brightness = (uint8_t) getOpt(_pin_lev, 0, 255);
+  setBrightness(brightness);
+}
+
+/*
+  Input a value 0 to 255 to get a color value.
+  The colors are a transition r - g - b - back to r.
+ */
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+/*
+  Input a value 0 to 255 to get a color value.
+  The colors are a transition r - g - b - back to r.
+ */
+uint32_t Wheel2(byte WheelPos) {
+  if(WheelPos < 128) {
+   return color(255 - WheelPos * 2, 0, WheelPos * 2);
+  } else {
+   WheelPos -= 128;
+   return color(WheelPos * 2, 0, 255 - WheelPos * 2);
+  }
+}
+
+/*
+  Fade from off to given color and back.
+ */
+void pulse(int w) {
+  uint32_t c;
+  //uint32_t c = Wheel(map(opt, 0, 1023, 0, 255)); 
+  for(uint16_t j = 8; j <= getOpt(_pin_lev, 0, 255); j++) {
+    if( checkButton() ){ return; };
+    setBrightness(j);
+    c = Wheel(map(opt, 0, 1023, 0, 255));
+    for(uint16_t i = 0; i < numPixels(); i++) {
+      setPixelColor(i, c);
+    }
+    show();
+    delay(w);
+  }
+  delay(w*10+50);
+  for(uint16_t j=getOpt(_pin_lev, 0, 255); j>8; j--) {
+    if( checkButton() ){ return; };
+    setBrightness(j);
+    c = Wheel(map(opt, 0, 1023, 0, 255));
+    for(uint16_t i = 0; i < numPixels(); i++) {
+      setPixelColor(i, c);
+    }
+    show();
+    delay(w);
+  }
+  delay(w*10+50);
+}
+
+/*
+  Rainbow Cycle Program - Equally distributed
+ */
+void rainbowCycle() {
+  setBrightness();
+  uint16_t i, j;
+  int j2;
+  for(j=0; j<256; j++) { // cycle all wheel colors
+
+    // calculate wait based on volatile opt from interrupt
+
+    // This includes a very hacky way to have the color direction reverse
+    // while maintaining the old "negative wait" convention to skip colors.
+    // As it stands, we don't invert j2 till it reaches 0 otherwise colors would
+    // suddenly change, but the direction may reverse a bit late (speeds up 
+    // before reversing).
+    int wait = (int) map(opt, 0, 1023, -200, 200);
+    if( wait < 0 ) {
+      if( j2 > 0 || j == 1 ){
+        j2 = j;
+      } else {
+        j2 = -j;
+      }
+      wait = wait + 100;
+    } else {
+      if( j2 < 0 || j == 1 ){
+        j2 = -j;
+      } else {
+        j2 = j;
+      }
+      wait = -wait + 100;
+    }
+    if( wait < 0 ) {
+      // negative values make a huge apparent difference, so fake a larger spread
+      // by having -100 to 100 instead of -20 to 100
+      wait = floor( wait / 5 );
+    }
+    // kind of a hack to allow negative wait value to speed rainbow by
+    // skipping colors, otherwise 0 would be fast as chip could process
+    if(wait < 0 &&  j % ( -1 * wait ) != 0){
+      continue; 
+    }
+
+    if( checkButton() ){ return; };
+    for(i = 0; i < numPixels(); i++) {
+      setPixelColor(
+        i,
+        Wheel(
+          // i*256/numPixels() is like mapping i in 0...numPixels() to 0..256
+          // 
+          // + j & 255 ensures we change color each time we visit given pixel
+          // while not running over 255
+          // 
+          // casting to uint32_t lets us handle strips longer than 256 pixels
+          // since that would result in number larger than 32K, causing wraparound
+          // to negative number
+          ( (uint8_t) ( (uint32_t) i * 256 / numPixels()/3) + j2) & 255
+        )
+      );
+    }
+    show();
+
+    if(wait > 0){
+      delay(wait);
+    }
+
+  }
+}
+
+/*
+  Rainbow Cycle Program - Equally distributed
+ */
+void rainbowCycle2() {
+  setBrightness();
+  uint16_t i, j;
+  int j2;
+  for(j=0; j<256; j++) { // cycle all wheel colors
+
+    // calculate wait based on volatile opt from interrupt
+
+    // This includes a very hacky way to have the color direction reverse
+    // while maintaining the old "negative wait" convention to skip colors.
+    // As it stands, we don't invert j2 till it reaches 0 otherwise colors would
+    // suddenly change, but the direction may reverse a bit late (speeds up 
+    // before reversing).
+    int wait = (int) map(opt, 0, 1023, -200, 200);
+    if( wait < 0 ) {
+      if( j2 > 0 || j == 1 ){
+        j2 = j;
+      } else {
+        j2 = -j;
+      }
+      wait = wait + 100;
+    } else {
+      if( j2 < 0 || j == 1 ){
+        j2 = -j;
+      } else {
+        j2 = j;
+      }
+      wait = -wait + 100;
+    }
+    if( wait < 0 ) {
+      // negative values make a huge apparent difference, so fake a larger spread
+      // by having -100 to 100 instead of -20 to 100
+      wait = floor( wait / 5 );
+    }
+    // kind of a hack to allow negative wait value to speed rainbow by
+    // skipping colors, otherwise 0 would be fast as chip could process
+    if(wait < 0 &&  j % ( -1 * wait ) != 0){
+      continue; 
+    }
+
+    if( checkButton() ){ return; };
+    for(i = 0; i < numPixels(); i++) {
+      setPixelColor(
+        i,
+        Wheel2(
+          // i*256/numPixels() is like mapping i in 0...numPixels() to 0..256
+          // 
+          // + j & 255 ensures we change color each time we visit given pixel
+          // while not running over 255
+          // 
+          // casting to uint32_t lets us handle strips longer than 256 pixels
+          // since that would result in number larger than 32K, causing wraparound
+          // to negative number
+          ( (uint8_t) ( (uint32_t) i * 256 / numPixels()/3) + j2) & 255
+        )
+      );
+    }
+    show();
+
+    if(wait > 0){
+      delay(wait);
+    }
+
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i = 0; i < numPixels(); i++) {
+    setPixelColor(i, c);
+    if ( wait ) {
+      show();
+      delay(wait);
+    }
+  }
+  if ( wait == 0 ) {
+    show();
+  }
+}
+
+//Rainbow Program
+void rainbowSingle() {
+  setBrightness();
+  //int wait = getOpt(_pin_opt, -20, 20);
+  uint16_t i, j;
+  for(j = 0; j < 256; j++) {
+
+    // calculate wait based on volatile opt from interrupt
+    int wait = (int) map(opt, 0, 1023, -100, 100);
+    if( wait < 0 ) {
+      // negative values make a huge apparent difference, so fake a larger spread
+      // by having -100 to 100 instead of -20 to 100
+      wait = floor( wait / 5 );
+    }
+    // kind of a hack to allow negative wait value to speed rainbow by
+    // skipping colors, otherwise 0 would be fast as chip could process
+    if(wait < 0 &&  j % ( -1 * wait ) != 0){
+      continue; 
+    }
+
+    if( checkButton() ){ return; };
+    for(i=0; i < numPixels(); i++) {
+      setPixelColor(i, Wheel(j));
+    }
+    show();
+
+    if(wait > 0){
+      delay(wait);
+    }
+
+  }
+}
+
+//Rainbow Program
+void rainbow() {
+  int wait = getOpt(_pin_opt, 0, 255);
+  uint16_t i, j;
+  for(j = 0; j < 256; j++) {
+    if( checkButton() ){ return; };
+    for(i = 0; i < numPixels(); i++) {
+      setPixelColor(i, Wheel((i + j) & 255));
+    }
+    show();
+    delay(wait);
+  }
+}
